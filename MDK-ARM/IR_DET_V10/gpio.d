d578fb9dab374a44398a6de197755b3dca30243c
cathyhoutt <cathyhoutt@users.noreply.github.com>
2026-07-08 10:14:22 +0800

    Initial commit

---
+++ MDK-ARM/IR_DET_V10/gpio.d
@@ -0,0 +1,31 @@
+ir_det_v10\gpio.o: ../Core/Src/gpio.c
+ir_det_v10\gpio.o: ../Core/Inc/gpio.h
+ir_det_v10\gpio.o: ../Core/Inc/main.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+ir_det_v10\gpio.o: ../Core/Inc/stm32f1xx_hal_conf.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+ir_det_v10\gpio.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+ir_det_v10\gpio.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h
+ir_det_v10\gpio.o: ../Drivers/CMSIS/Include/core_cm3.h
+ir_det_v10\gpio.o: D:\Program Files\KEIL5\ARM\ARMCC\Bin\..\include\stdint.h
+ir_det_v10\gpio.o: ../Drivers/CMSIS/Include/cmsis_version.h
+ir_det_v10\gpio.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+ir_det_v10\gpio.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+ir_det_v10\gpio.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+ir_det_v10\gpio.o: D:\Program Files\KEIL5\ARM\ARMCC\Bin\..\include\stddef.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+ir_det_v10\gpio.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
---
+++ MDK-ARM/IR_DET_V10/startup_stm32f103xe.d
@@ -0,0 +1,1 @@
+ir_det_v10\startup_stm32f103xe.o: startup_stm32f103xe.s
